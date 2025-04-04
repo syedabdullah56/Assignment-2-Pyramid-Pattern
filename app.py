12e46986bb95e4a1f4365b6b609625055dc53919
syedabdullah56 <syedabdullah8750@gmail.com>
2025-04-05 01:20:58 +0500

    Built

---
+++ app.py
@@ -0,0 +1,14 @@
+# Pyramid Pattern
+
+n=5
+for i in range(1,n+1):
+    #Printing Spaces
+    print(" "*(n-i),end="")
+    #Printing Stars
+    print("*"*(2*i-1))
+# output:
+#     *
+#    ***
+#   *****
+#  *******
+# *********
